291f569dcc014801a8d9c3ae5501cee3eb91dcc8
Yuki Shimizu <yuki.shimizu.contact@gmail.com>
2024-01-24 16:21:20 +0900

    export treeMap

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export { TreeFactory } from "./tree.factory";
+export { TreeFactory, treeMap } from "./tree.factory";
 export { type NodeParam, type TreeType, Tree } from "./tree";
 export { Node } from "./node";
