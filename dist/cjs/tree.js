2eee7a2f9f382a81efcf0da86fab78b2226a94b8
Yuki Shimizu <yuki.shimizu.contact@gmail.com>
2024-03-13 17:37:21 +0900

    bun run build

--- dist/cjs/tree.js
+++ dist/cjs/tree.js
@@ -1,115 +1,116 @@
-"use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Tree = void 0;
-const crypto_1 = require("crypto");
+const uuid_1 = require("uuid");
 const node_1 = require("./node");
 class Tree {
-    treeId;
-    root;
-    constructor({ nodes, key, childKey, }) {
-        this.treeId = (0, crypto_1.randomUUID)();
-        const nodeParam = this.#buildParams({
-            nodes,
-            key,
-            childKey,
-        });
-        this.root = this.#parse({
-            key,
-            childKey,
-            nodeParam,
-        });
+  treeId;
+  root;
+  constructor({ nodes, key, childKey }) {
+    this.treeId = (0, uuid_1.v4)();
+    const nodeParam = this.#buildParams({
+      nodes,
+      key,
+      childKey,
+    });
+    this.root = this.#parse({
+      key,
+      childKey,
+      nodeParam,
+    });
+  }
+  find(fn) {
+    let targetNode = undefined;
+    this.root.move((node) => {
+      if (fn(node)) {
+        targetNode = node;
+        return false;
+      }
+      return true;
+    });
+    return targetNode;
+  }
+  findOrThrow(fn) {
+    const targetNode = this.find(fn);
+    if (!targetNode) {
+      throw new Error("Target node not found");
     }
-    find(fn) {
-        let targetNode = undefined;
-        this.root.move((node) => {
-            if (fn(node)) {
-                targetNode = node;
-                return false;
-            }
-            return true;
-        });
-        return targetNode;
+    return targetNode;
+  }
+  findMany(fn) {
+    const targetNodes = [];
+    this.root.move((node) => {
+      if (fn(node)) {
+        targetNodes.push(node);
+      }
+      return true;
+    });
+    return targetNodes;
+  }
+  flat() {
+    const nodes = [];
+    this.root.move((node) => {
+      nodes.push(node);
+      return true;
+    });
+    return nodes;
+  }
+  #buildParams({ nodes, key, childKey }) {
+    const nodeParams = nodes.map((node) => ({
+      ...node,
+      children: [],
+    }));
+    const nodeMap = new Map();
+    const rootNode = nodeParams.find((node) => node.level === 0);
+    if (!rootNode) {
+      throw new Error("Level 0 root node not found");
     }
-    findOrThrow(fn) {
-        const targetNode = this.find(fn);
-        if (!targetNode) {
-            throw new Error("Target node not found");
+    for (const node of nodeParams) {
+      nodeMap.set(node.data[key], {
+        ...node,
+        children: [],
+        parentKey: null,
+      });
+    }
+    for (const node of nodeParams) {
+      if (node.data[childKey]) {
+        const parent = nodeMap.get(node.data[key]);
+        if (!parent) {
+          throw new Error("Parent node not found");
         }
-        return targetNode;
-    }
-    findMany(fn) {
-        const targetNodes = [];
-        this.root.move((node) => {
-            if (fn(node)) {
-                targetNodes.push(node);
-            }
-            return true;
+        const child = nodeMap.get(node.data[childKey]);
+        if (!child) {
+          throw new Error("Child node not found");
+        }
+        parent?.children.push({
+          ...child,
+          parentKey: parent.data[key],
         });
-        return targetNodes;
+      }
     }
-    flat() {
-        const nodes = [];
-        this.root.move((node) => {
-            nodes.push(node);
-            return true;
-        });
-        return nodes;
+    const root = nodeMap.get(rootNode?.data[key]);
+    for (const node of nodeParams) {
+      delete node.data[childKey];
     }
-    #buildParams({ nodes, key, childKey, }) {
-        const newNodes = nodes.map((node) => ({
-            ...node,
-            children: [],
-        }));
-        const nodeMap = new Map();
-        const rootNode = newNodes.find((node) => node.level === 0);
-        if (!rootNode) {
-            throw new Error("Level 0 root node not found");
-        }
-        for (const node of newNodes) {
-            nodeMap.set(node.data[key], {
-                ...node,
-                children: [],
-                parentKey: null,
-            });
-        }
-        for (const node of newNodes) {
-            if (node.data[childKey]) {
-                const parent = nodeMap.get(node.data[key]);
-                if (!parent) {
-                    throw new Error("Parent node not found");
-                }
-                const child = nodeMap.get(node.data[childKey]);
-                if (!child) {
-                    throw new Error("Child node not found");
-                }
-                parent?.children.push({
-                    ...child,
-                    parentKey: parent.data[key],
-                });
-            }
-        }
-        const root = nodeMap.get(rootNode?.data[key]);
-        for (const node of newNodes) {
-            delete node.data[childKey];
-        }
-        return root;
-    }
-    #parse({ key, childKey, nodeParam, }) {
-        const node = new node_1.Node({
-            treeId: this.treeId,
-            key,
-            childKey,
-            level: nodeParam.level,
-            parentKey: nodeParam.parentKey,
-            children: nodeParam.children.map((childNodeModel) => this.#parse({
-                key,
-                childKey,
-                nodeParam: childNodeModel,
-            })),
-            data: nodeParam.data,
-        });
-        return node;
-    }
+    return root;
+  }
+  #parse({ key, childKey, nodeParam }) {
+    const node = new node_1.Node({
+      treeId: this.treeId,
+      key,
+      childKey,
+      level: nodeParam.level,
+      parentKey: nodeParam.parentKey,
+      children: nodeParam.children.map((childNodeModel) =>
+        this.#parse({
+          key,
+          childKey,
+          nodeParam: childNodeModel,
+        }),
+      ),
+      data: nodeParam.data,
+    });
+    return node;
+  }
 }
 exports.Tree = Tree;
 //# sourceMappingURL=tree.js.map
--- dist/esm/tree.factory.d.ts
+++ dist/esm/tree.factory.d.ts
@@ -1,4 +1,4 @@
-import { type Data, Tree } from "./tree";
+import type { Data, Tree } from "./tree";
 export declare const treeMap: Map<string, unknown>;
 export declare const TreeFactory: {
   create<T extends Data, K extends keyof T>({
